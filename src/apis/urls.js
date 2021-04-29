a78f387b5719d40e8ddeb6160844ff3c6a42b0fe
alfaroelias001 <77286773+alfaroelias001@users.noreply.github.com>
2021-04-29 08:25:41 -0300

    correción url

--- src/apis/urls.js
+++ src/apis/urls.js
@@ -4,7 +4,6 @@ export const MAIN_URL = "https://lafersegurosapi.azurewebsites.net/";
 --- URL CUANDO EL REPO SE ENCUENTRA EN VM.
 export const MAIN_URL = "https://localhost:44304/";
 */
-// export const MAIN_URL = "https://lafersegurosapi.azurewebsites.net/";
-export const MAIN_URL = "https://localhost:44304/";
+export const MAIN_URL = "https://lafersegurosapi.azurewebsites.net/";
 
 export const API_URL = `${MAIN_URL}api/v1.0/`;
